f37540f0bd9b5c81313e8629bbdc8864ab8f4ff1
DwarfPorter <mrvlmor@gmail.com>
2021-11-16 20:39:12 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/account/MainActivity.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        Button bthGreeting = findViewById(R.id.btnGreetings);
+        EditText txtName = findViewById(R.id.textName);
+        TextView txtHello = findViewById(R.id.textHello);
+        bthGreeting.setOnClickListener(view -> {
+            String name = txtName.getText().toString();
+            String sayHello = getString(R.string.say_hello) + name;
+            txtHello.setText(sayHello);
+        });
+    }
+}
